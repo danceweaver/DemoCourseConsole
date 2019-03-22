4d76741a1b0eb2a42a721c6cb042667d7c764801
test.class <testclass@isiinc.com>
2019-03-22 13:39:28 -0700

    Added Initial file och git ignore

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DemoConsoleLab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
